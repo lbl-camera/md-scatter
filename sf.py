b3e1c3725664831ce8b6e7e5d5b156eaaf0b652d
Dinesh Kumar <dkumar@lbl.gov>
2020-02-21 15:30:15 -0800

    using gpu to calculated dft

--- sf.py
+++ sf.py
@@ -1,9 +1,21 @@
 #! /usr/bin/env python
 
 import numpy as np
+try:
+    import mdscatter
+    have_ext = True
+except ImportError as e:
+    have_ext = False
+    print('Failed to import mdscatter')
 
 def structure_factor(qx, qy, qz, locations):
-    sf = np.zeros(qx.shape, dtype=np.complex) 
-    for loc in locations:
-        sf += np.exp(-1j * (qx * loc[0] + qy * loc[1] + qz * loc[2]))
-    return sf
+
+    if not have_ext:
+        sf = np.zeros(qx.shape, dtype=np.complex) 
+        for loc in locations:
+            sf += np.exp(-1j * (qx * loc[0] + qy * loc[1] + qz * loc[2]))
+    else:
+        shape = qx.shape
+        qvals = np.array([qx.ravel(), qy.ravel(), qz.ravel()]).T
+        sf = mdscatter.gpudft(locations, qvals)
+        return sf.reshape(shape)
