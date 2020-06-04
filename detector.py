85c652240a915cb86372c1a00d54bf0f083a3f44
Dinesh Kumar <dkumar@lbl.gov>
2020-06-04 11:34:04 -0700

    Added a goniometer

--- detector.py
+++ detector.py
@@ -8,7 +8,16 @@ class Detector:
         self.shape = shape
         self.pixle_size = pixle_size
 
-    def qvectors(self, sdd, center, energy):
+    def qvectors(self, sdd, center, energy, alpha=0., theta=0.):
+
+
+        # detector angles
+        cos_a = np.cos(alpha)
+        sin_a = np.sin(alpha)
+        cos_t = np.cos(theta)
+        sin_t = np.sin(theta)
+
+        # gridding
         nrow = self.shape[0]
         ncol = self.shape[1]
         y, x = np.mgrid[0:nrow, 0:ncol]
@@ -20,16 +29,17 @@ class Detector:
         # angles 
         # theta
         tmp = np.sqrt(x**2 + sdd**2)
-        cos_th = sdd / tmp
-        sin_th = x / tmp
+        cos_th = cos_t * (sdd / tmp) - sin_t * (x / tmp)
+        sin_th = cos_t * (x / tmp) + sin_t * (sdd / tmp)
 
         # alpha
         tmp2 = np.sqrt(y**2 + tmp**2)
-        cos_al = tmp / tmp2
-        sin_al = y / tmp2
+        cos_al = cos_a * (tmp / tmp2) - sin_a * (y / tmp2)
+        sin_al = cos_a * (y / tmp2) + sin_a * (tmp / tmp2)
 
         # radius of the Ewald's sphere
-        k0 = 2 * np.pi * energy / 1.23984
+        wave = 0.123984E+04 / energy
+        k0 = 2 * np.pi / wave
 
         # q-vector
         qx = k0 * (cos_al * cos_th - 1)
@@ -37,8 +47,8 @@ class Detector:
         qz = k0 * (sin_al)
         return qx, qy, qz
 
-    def qvalues(self, sdd, center, energy):
-        qx, qy, qz = self.qvectors(sdd, center, energy)
+    def qvalues(self, sdd, center, energy, alpha=0., theta=0.):
+        qx, qy, qz = self.qvectors(sdd, center, energy, alpha, theta)
         return np.sqrt(qx**2 + qy**2 + qz**2)
 
 
@@ -58,3 +68,9 @@ class Pilatus1M(Detector):
         self.name = 'Pilatus 1M'
         self.shape = (1043, 981)
         self.pixle_size = (172.E-06, 172.E-06)
+
+class ImpossibleDet(Detector):
+    def __init__(self):
+        self.name = 'Impossible Detector'
+        self.shape = (1024, 1024)
+        self.pixle_size = (50.E-06, 50.E-06)
